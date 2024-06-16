7f7a6c2705a3d0e0f6c94764e7abccb416a1d65a
Jaeger Yan <appie40@outlook.com>
2024-06-16 21:08:52 +0800

    Project Sprint 2,Modify Sql

--- Project Sprint 2/JOIN Operations.sql
+++ Project Sprint 2/JOIN Operations.sql
@@ -3,8 +3,8 @@
 -- We can use an INNER JOIN to combine the data from these tables based on the customer_id column.
 SELECT c.customer_id, c.first_name, c.last_name, SUM(t.amount) as total_spent
 FROM Customers c
-INNER JOIN Rentals r ON c.customer_id = r.customer_id
-INNER JOIN Transactions t ON r.rental_id = t.rental_id 
+         INNER JOIN Rentals r ON c.customer_id = r.customer_id
+         INNER JOIN Transactions t ON r.rental_id = t.rental_id
 GROUP BY c.customer_id
 ORDER BY customer_id;
 
@@ -12,9 +12,9 @@ ORDER BY customer_id;
 -- we can use an INNER JOIN to combine the Customers, Rentals, Inventory, and Consoles tables.
 SELECT c.customer_id, c.first_name, c.last_name, COUNT(DISTINCT i.console_id) AS number_of_different_consoles_rented
 FROM Customers c
-INNER JOIN Rentals r ON c.customer_id = r.customer_id
-INNER JOIN Inventory i ON r.inventory_id = i.inventory_id
-INNER JOIN Consoles con ON i.console_id = con.console_id
+         INNER JOIN Rentals r ON c.customer_id = r.customer_id
+         INNER JOIN Inventory i ON r.inventory_id = i.inventory_id
+         INNER JOIN Consoles con ON i.console_id = con.console_id
 GROUP BY c.customer_id;
 
 -- LEFT JOIN
@@ -22,24 +22,24 @@ GROUP BY c.customer_id;
 -- we need to join the Customers table with the Rentals and Transactions tables.
 SELECT cu.customer_id, SUM(total_cost) as total_cost
 FROM Customers cu
-LEFT JOIN rentals r ON cu.customer_id = r.customer_id
+         LEFT JOIN rentals r ON cu.customer_id = r.customer_id
 GROUP BY cu.customer_id
-ORDER BY customer_id
+ORDER BY customer_id;
 
 -- To find the total number of rentals for each customer,
 -- we can use a LEFT JOIN to include all customers even if they have not rented any consoles.
 SELECT count(*) as total_number, c.customer_id, c.first_name, c.last_name
 FROM Customers c
-LEFT JOIN 
-		Rentals r ON c.customer_id = r.customer_id
+         LEFT JOIN
+     Rentals r ON c.customer_id = r.customer_id
 GROUP BY c.customer_id;
 
 -- To find the most recent rental details for each console,
 -- we can use a LEFT JOIN to include all consoles even if they have not been rented.
 SELECT con.console_id, con.model, r.rental_start_date, r.rental_end_date
 FROM Consoles con
-LEFT JOIN Inventory i ON con.console_id = i.console_id
-LEFT JOIN Rentals r ON i.inventory_id = r.inventory_id
+         LEFT JOIN Inventory i ON con.console_id = i.console_id
+         LEFT JOIN Rentals r ON i.inventory_id = r.inventory_id
 ORDER BY con.console_id, r.rental_start_date DESC;
 
 -- RIGHT JOIN
@@ -47,6 +47,6 @@ ORDER BY con.console_id, r.rental_start_date DESC;
 -- we need to join the Customers table with the Rentals and Transactions tables.
 SELECT cu.customer_id, SUM(total_cost) as total_cost
 FROM Customers cu
-RIGHT JOIN rentals r ON cu.customer_id = r.customer_id
+         RIGHT JOIN rentals r ON cu.customer_id = r.customer_id
 GROUP BY cu.customer_id
 ORDER BY customer_id;
