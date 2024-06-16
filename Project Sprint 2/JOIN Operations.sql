e627a612c8061fb9b511ab0b710446b206747d77
NeilTsy <125690785+NeilTsy@users.noreply.github.com>
2024-06-16 10:01:38 +0800

    Add files via upload

--- Project Sprint 2/JOIN Operations.sql
+++ Project Sprint 2/JOIN Operations.sql
@@ -1,11 +1,12 @@
 -- INNER JOIN
 -- To find the total amount spent by each customer, we need to join the Customers table with the Rentals and Transactions tables.
 -- We can use an INNER JOIN to combine the data from these tables based on the customer_id column.
-SELECT c.customer_id, c.first_name, c.last_name, con.model
+SELECT c.customer_id, c.first_name, c.last_name, SUM(t.amount) as total_spent
 FROM Customers c
 INNER JOIN Rentals r ON c.customer_id = r.customer_id
-INNER JOIN Inventory i ON r.inventory_id = i.inventory_id
-INNER JOIN Consoles con ON i.console_id = con.console_id;
+INNER JOIN Transactions t ON r.rental_id = t.rental_id 
+GROUP BY c.customer_id
+ORDER BY customer_id;
 
 -- To find the total number of different consoles rented by each customer,
 -- we can use an INNER JOIN to combine the Customers, Rentals, Inventory, and Consoles tables.
@@ -19,20 +20,18 @@ GROUP BY c.customer_id;
 -- LEFT JOIN
 -- To find the total amount spent by each customer,
 -- we need to join the Customers table with the Rentals and Transactions tables.
-SELECT con.console_id, con.model, r.rental_id
-FROM Consoles con
-LEFT JOIN Inventory i ON con.console_id = i.console_id
-LEFT JOIN Rentals r ON i.inventory_id = r.inventory_id;
+SELECT cu.customer_id, SUM(total_cost) as total_cost
+FROM Customers cu
+LEFT JOIN rentals r ON cu.customer_id = r.customer_id
+GROUP BY cu.customer_id
+ORDER BY customer_id
 
 -- To find the total number of rentals for each customer,
 -- we can use a LEFT JOIN to include all customers even if they have not rented any consoles.
-SELECT c.customer_id, c.first_name, c.last_name, r.rental_start_date, r.rental_end_date
+SELECT count(*) as total_number, c.customer_id, c.first_name, c.last_name
 FROM Customers c
-LEFT JOIN (
-    SELECT *
-    FROM Rentals
-    ORDER BY rental_start_date DESC
-) r ON c.customer_id = r.customer_id
+LEFT JOIN 
+		Rentals r ON c.customer_id = r.customer_id
 GROUP BY c.customer_id;
 
 -- To find the most recent rental details for each console,
@@ -40,17 +39,14 @@ GROUP BY c.customer_id;
 SELECT con.console_id, con.model, r.rental_start_date, r.rental_end_date
 FROM Consoles con
 LEFT JOIN Inventory i ON con.console_id = i.console_id
-LEFT JOIN (
-    SELECT *
-    FROM Rentals
-    ORDER BY rental_start_date DESC
-) r ON i.inventory_id = r.inventory_id
-GROUP BY con.console_id;
+LEFT JOIN Rentals r ON i.inventory_id = r.inventory_id
+ORDER BY con.console_id, r.rental_start_date DESC;
 
 -- RIGHT JOIN
 -- To find the total amount spent by each customer,
 -- we need to join the Customers table with the Rentals and Transactions tables.
-SELECT con.console_id, con.model, r.rental_id
-FROM Consoles con
-RIGHT JOIN Inventory i ON con.console_id = i.console_id
-RIGHT JOIN Rentals r ON i.inventory_id = r.inventory_id;
+SELECT cu.customer_id, SUM(total_cost) as total_cost
+FROM Customers cu
+RIGHT JOIN rentals r ON cu.customer_id = r.customer_id
+GROUP BY cu.customer_id
+ORDER BY customer_id;
