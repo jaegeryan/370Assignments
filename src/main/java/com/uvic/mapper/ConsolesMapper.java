d95ae3985a42f1f4195db922c7d31559ffdcfd5f
Jaeger_yan <appie40@outlook.com>
2024-07-20 07:00:18 +0800

    Test git push

---
+++ src/main/java/com/uvic/mapper/ConsolesMapper.java
@@ -0,0 +1,18 @@
+package com.uvic.mapper;
+
+import com.uvic.entity.Consoles;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Jaegeryan
+* @description 针对表【consoles】的数据库操作Mapper
+* @createDate 2024-07-20 06:31:20
+* @Entity com.uvic.entity.Consoles
+*/
+public interface ConsolesMapper extends BaseMapper<Consoles> {
+
+}
+
+
+
+
