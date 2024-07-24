3236e0db6ff3dc8a4147abc9d10f6cd3c32d7b5e
Jaeger_yan <appie40@outlook.com>
2024-07-24 18:42:52 +0800

    Customers controller, and json format of Date

---
+++ src/main/java/com/uvic/entity/Consoles.java
@@ -0,0 +1,59 @@
+package com.uvic.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @TableName consoles
+ */
+@TableName(value ="consoles")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Consoles implements Serializable {
+    /**
+     *
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer consoleId;
+
+    /**
+     *
+     */
+    private String model;
+
+    /**
+     *
+     */
+    private String type;
+
+    /**
+     *
+     */
+    private String storageCapacity;
+
+    /**
+     *
+     */
+    private Date purchaseDate;
+
+    /**
+     *
+     */
+    private BigDecimal cost;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
